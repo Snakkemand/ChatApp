bab72b4fba380fad9056301e9625ca047452a52b
Sin Samini <ssamin17@student.aau.dk>
2018-10-24 13:51:34 +0200

    instantiated the main class ChatServer, and the attributes for the various methods

--- ChatApp/src/ChatServer.java
+++ ChatApp/src/ChatServer.java
@@ -1,3 +1,31 @@
+import java.io.*;
+import java.net.Socket;
+import java.net.ServerSocket;
 public class ChatServer {
-
-}
+    private static ServerSocket serverSocket = null;
+    private static Socket clientSocket = null;
+    private static final int maxClientsCount = 6;
+    private static final clientThread[] threads = new clientThread[maxClientsCount];
+    public static void main(String args[]) {
+        int portNumber = 6969;
+        if (args.length < 1) {
+            System.out.println("Usage: java MultiThreadChatServerSync <portNumber>\n"
+                    + "Now using port number=" + portNumber);
+        } else {
+            portNumber = Integer.valueOf(args[0]).intValue();
+        }
+        try {
+            serverSocket = new ServerSocket(portNumber);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+        while (true) {
+            try {
+                clientSocket = serverSocket.accept();
+                int i;
+                for (i = 0; i < maxClientsCount; i++) {
+                    if (threads[i] == null) {
+                        (threads[i] = new clientThread(clientSocket, threads)).start();
+                        break;
+                    }
+                }
